abb560ec99d5abe55f16d10091d980db0dccb0c3
Alex Farmer <farmale10@gmail.com>
2019-08-30 18:34:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,17 +1,20 @@
 
+def map(element1,&block); 
+  element1.map(&block); 
+  end
 
-def map(array, &block)
-  i = 0
-  def what_is_i
-    yield array[i]
-  end
-  new_array = []
-  while array.length > i do
-    new_array.push(what_is_i &block)
-    i += 1
-  end
+#def map(array, &block)
+#  i = 0
+#  def what_is_i
+#    yield array[i]
+#  end
+#  new_array = []
+#  while array.length > i do
+#    new_array.push(what_is_i &block)
+#    i += 1
+#  end
   
-end
+#end
 
 
 
