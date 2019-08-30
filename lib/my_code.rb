dd66ee4458cfb4d245d1edc4ebc88e0e71e76fdb
Alex Farmer <farmale10@gmail.com>
2019-08-30 18:46:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,10 +3,10 @@ def map(array,&block);
   array.map(&block); 
   end
 
-def reduce(elements, starting_point = nil, &block)
+def reduce(array, starting_point = nil, &block)
   if starting_point.nil?
-    elements.reduce(&block)
+    array.reduce(&block)
   else
-    elements.reduce(starting_point, &block)
+    array.reduce(starting_point, &block)
   end
 end
