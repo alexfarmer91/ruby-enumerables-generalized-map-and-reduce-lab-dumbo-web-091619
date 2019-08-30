18138442db3179cf584476eca01cf75e77a3297c
Alex Farmer <farmale10@gmail.com>
2019-08-30 17:39:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,14 @@ def my_own_map(array, block)
 
 
 
-
+def map_to_negativize(array, block)
+  i = 0
+  while source_array.length > i do
+    source_array[i] *= -1
+    i += 1
+  end
+  p source_array
+end
 
 
 
